bf8334c61373303b2be6a927f87f0f9edee1cc5e
betotramontina <122474917+betotramontina@users.noreply.github.com>
2024-11-22 23:13:02 -0300

    Create __init__.py

---
+++ schemas/__init__.py
@@ -0,0 +1,4 @@
+from schemas.contato import ContatoSchema, ContatoBuscaSchema, ContatoViewSchema, \
+                            ListagemContatosSchema, ContatoDelSchema, apresenta_contatos, \
+                            apresenta_contato, apresenta_contatos
+from schemas.error import ErrorSchema
