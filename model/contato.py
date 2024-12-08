2df50ec039ff92943463c987258aae9c095389ce
betotramontina <122474917+betotramontina@users.noreply.github.com>
2024-12-08 18:09:25 -0300

    Update contato.py

--- model/contato.py
+++ model/contato.py
@@ -12,12 +12,11 @@ class Contato(Base):
     id = Column("pk_contato", Integer, primary_key=True)
     nome = Column(String(50), unique=True)
     celular = Column(String(11))
-    email = Column(String(50))
     data_nascimento = Column(String(10))
     data_insercao = Column(DateTime, default=datetime.now())
 
     
-    def __init__(self, nome:str, celular:str, email:str, data_nascimento:str,
+    def __init__(self, nome:str, celular:str, data_nascimento:str,
                  data_insercao:Union[DateTime, None] = None):
         """
         Cria um Contato
@@ -25,13 +24,11 @@ class Contato(Base):
         Arguments:
             nome: nome do contato.
             celular: número do celular do contato
-            email: email do contato
             data_nascimento: data de nascimento/ aniversário do contato
             data_insercao: data de inserção do contato na base de dados
         """
         self.nome = nome
         self.celular = celular
-        self.email = email
         self.data_nascimento = data_nascimento
 
         # se não for informada, será o data exata da inserção no banco
