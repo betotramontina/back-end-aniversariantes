3ee0ec5e0f9cd718a7c17df5c5a8263f0693d612
betotramontina <122474917+betotramontina@users.noreply.github.com>
2024-12-10 16:29:23 -0300

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,30 +1,33 @@
+# Importa funções, ferramentas e módulos
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
 
-# importando os elementos definidos no modelo
+# Importa os elementos definidos no modelo
 from model.base import Base
 from model.contato import Contato
 
+# Define o caminho para o diretório do banco de dados
 db_path = "database/"
-# Verifica se o diretorio não existe
+# Verifica se o diretório especificado para o banco de dados não existe
 if not os.path.exists(db_path):
-   # então cria o diretorio
+   # Cria o diretório do banco de dados, caso ele não exista
    os.makedirs(db_path)
 
-# url de acesso ao banco (essa é uma url de acesso ao sqlite local)
+# Define a URL de conexão com o banco de dados (SQLite local)
 db_url = 'sqlite:///%s/db.sqlite3' % db_path
 
-# cria a engine de conexão com o banco
+# Cria a engine de conexão com o banco de dados
 engine = create_engine(db_url, echo=False)
 
-# Instancia um criador de seção com o banco
+# Instancia um criador de sessões para interagir com o banco de dados
 Session = sessionmaker(bind=engine)
 
-# cria o banco se ele não existir 
+# Verifica se o banco de dados não existe
 if not database_exists(engine.url):
+    # Cria o banco de dados, caso ele ainda não tenha sido criado
     create_database(engine.url) 
 
-# cria as tabelas do banco, caso não existam
+# Cria as tabelas no banco de dados com base no modelo, caso ainda não existam
 Base.metadata.create_all(engine)
