eee09d527d1e3b7f1fea5f7db5ed058c7040c049
betotramontina <122474917+betotramontina@users.noreply.github.com>
2024-12-10 16:48:36 -0300

    Update contato.py

--- schemas/contato.py
+++ schemas/contato.py
@@ -2,32 +2,22 @@ from pydantic import BaseModel
 from typing import Optional, List
 from model.contato import Contato
 
-
+# Define como um novo contato deve ser representado ao ser inserido.
 class ContatoSchema(BaseModel):
-    """ Define como um novo contato a ser inserido deve ser representado
-    """
     nome: str = "Carlos Araujo"
     celular: str = "61990070205"
     data_nascimento: str = "DD-MM-AAAA" 
 
+# Define a estrutura da busca de um contato, que será realizada apenas com base no nome.
+ class ContatoBuscaSchema(BaseModel):
+     nome: str = "Carlos Araujo"
 
-class ContatoBuscaSchema(BaseModel):
-    """ Define como deve ser a estrutura que representa a busca. Que será
-        feita apenas com base no nome do contato.
-    """
-    nome: str = "Carlos Araujo"
-
-
+# Define como uma lista de contatos será retornada na resposta da API.
 class ListagemContatosSchema(BaseModel):
-    """ Define como uma listagem de contatos será retornada.
-    """
     contatos:List[ContatoSchema]
 
-
+# Converte uma lista de objetos do modelo Contato para uma estrutura de dicionário que segue o formato definido pelo esquema ListagemContatosSchema.
 def apresenta_contatos(contatos: List[Contato]):
-    """ Retorna uma representação do contato seguindo o schema definido em
-        ContatoViewSchema.
-    """
     result = []
     for contato in contatos:
         result.append({
@@ -38,27 +28,20 @@ def apresenta_contatos(contatos: List[Contato]):
 
     return {"contatos": result}
 
-
+# Define como os detalhes de um contato específico serão retornados na API.
 class ContatoViewSchema(BaseModel):
-    """ Define como um contato será retornado: contato.
-    """
     id: int = 1
     nome: str = "Carlos Araujo"
     celular: str = "61990070205"
     data_nascimento: str = "DD-MM-AAAA" 
 
-
+# Define a estrutura do dado retornado após a remoção de um contato.
 class ContatoDelSchema(BaseModel):
-    """ Define como deve ser a estrutura do dado retornado após uma requisição
-        de remoção.
-    """
     message: str
     nome: str
 
+# Converte um objeto do modelo Contato para uma representação compatível com o esquema definido em ContatoViewSchema.
 def apresenta_contato(contato: Contato):
-    """ Retorna uma representação do contato seguindo o schema definido em
-        ContatoViewSchema.
-    """
     return {
         "id": contato.id,
         "nome": contato.nome,
