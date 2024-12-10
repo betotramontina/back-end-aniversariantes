61cd0ab0af570033a7d09ceb5037277ab45f9637
betotramontina <122474917+betotramontina@users.noreply.github.com>
2024-12-10 17:28:49 -0300

    Update error.py

--- schemas/error.py
+++ schemas/error.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
-# Define como uma mensagem de erro será representada.
 class ErrorSchema(BaseModel):
+""" Define como uma mensagem de erro será representada.
+"""
     message: str
